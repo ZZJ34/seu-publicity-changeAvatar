d175a201d80f798a83b602c2837a47f4eca4b55e
ZZJ <zhaozjseu@163.com>
2020-05-18 11:03:26 +0800

    改回去

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  publicPath:  process.env.NODE_ENV === 'production'
-  ? 'https://cdn.xgbxscwx.myseu.cn/avatar/'
-  : '',
+  // publicPath:  process.env.NODE_ENV === 'production'
+  // ? 'https://cdn.xgbxscwx.myseu.cn/avatar/'
+  // : '',
+  publicPath: ''
 }
